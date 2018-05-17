b4c1922c9ea062c97674f93ae2cbace99e83bdff
Gabriel Minatto <gabriel.minatto@deliverit.com.br>
2018-05-17 18:46:49 -0300

    Refatoracao do codigo que insere nodos no project steps.

--- client/views/private/projects/projectSteps.js
+++ client/views/private/projects/projectSteps.js
@@ -1,6 +1,7 @@
 Template.projectSteps.onCreated(function() {
 
     this.threeNodes = new ReactiveVar(false);
+    this.nodeStructure = new ReactiveVar(false);
 
     subsGlobal.subscribe('publishedProjects');
     subsGlobal.subscribe('activeUserSteps');
@@ -14,10 +15,11 @@ Template.projectSteps.onCreated(function() {
 
         const id = FlowRouter.getParam('id');
 
-        const project = Projects.findOne({
-            _id: id
-        });
+        const project = Projects.findOne({ _id: id });
 
+        const nodeStructure = tranforma(project.steps).em().diagrama();
+
+        self.nodeStructure.set(nodeStructure);
         self.threeNodes.set(project.steps);
     });
 })
@@ -30,65 +32,23 @@ Template.projectSteps.onRendered(function() {
 
         if (!subsGlobal.ready()) return;
 
-        const threeNodes = self.threeNodes.get();
-
-        const tranforma = (threeNodes) => {
-            return {
-                em() {
-                    return {
-                        diagrama() {
-                            const mapNode = threeNodes.map((val, ind, arr) => {
-                                const nodo = val.stepNode;
-                                const nodeControls = {
-                                    left: (threeNodes[0].fatherId ? true : false),
-                                    right: (threeNodes[0].fatherId ? true : false),
-                                    down: (!nodo.children),
-                                    fatherId: val.fatherId,
-                                    threeId: val.threeId
-                                };
-                                const newNode = {
-                                    innerHTML: UI.toHTMLWithData(Template.stepBox, {
-                                        step: nodo,
-                                        routeName: 'steps-edit',
-                                        nodeControls: nodeControls
-                                    })
-                                }
-
-                                if (!nodo.children) return newNode;
-
-                                newNode.children = tranforma(nodo.children).em().diagrama();
-
-                                return newNode;
-                            });
-                            if (!threeNodes[0].fatherId) return mapNode[0];
-
-                            return mapNode;
-                        }
-                    }
-                }
-            }
-        };
-
-        const nodeStructure = tranforma(threeNodes).em().diagrama();
+        const nodeStructure = self.nodeStructure.get();
 
         const chart_config = {
             chart: {
                 container: "#stepsContainer",
-
                 connectors: {
                     type: 'step'
                 },
-
                 node: {
                     HTMLclass: 'step-node'
                 },
             },
-
             nodeStructure: nodeStructure
 
         };
 
-        var chart = new Treant(chart_config, null, $);
+        new Treant(chart_config, null, $);
     });
 
 });
@@ -108,56 +68,6 @@ Template.projectSteps.events({
         const threeNodes = Template.instance().threeNodes.get();
         const fatherId = e.target.dataset.stepid;
         const push = e.target.dataset.push;
-
-        const procura = (fatherId) => {
-            return {
-                em(threeNodes) {
-                    return {
-                        insere(newNode, push) {
-                            const newThree = [];
-                            let stop = false;
-                            threeNodes.forEach((val, ind) => {
-
-                                if(stop) return newThree.push(val);
-
-                                if(val.threeId == fatherId){
-
-                                    if(!val.stepNode.children){
-                                        val.stepNode.children = [];
-                                    }
-
-                                    if(push) {
-                                        val.stepNode.children.push(newNode);
-                                    }
-                                    else {
-                                        val.stepNode.children.unshift(newNode);
-                                    }
-
-                                    newThree.push(val);
-                                    stop = true;
-                                    return;
-
-                                }
-
-                                if (!val.stepNode.children){
-                                    newThree.push(val);
-                                    return;
-                                }
-
-                                val.stepNode.children = procura(fatherId)
-                                    .em(val.stepNode.children)
-                                    .insere(newNode, push);
-
-                                newThree.push(val);
-
-                            });
-
-                            return newThree;
-                        }
-                    }
-                }
-            }
-        };
 
         const step = Steps.findOne({_id:"2GvPtnHXR82hJwsGx"});
         const newChild = {
@@ -184,3 +94,90 @@ Template.projectSteps.events({
 
     }
 });
+
+function tranforma(threeNodes) {
+    return {
+        em() {
+            return {
+                diagrama() {
+                    const mapNode = threeNodes.map((val, ind, arr) => {
+                        const nodo = val.stepNode;
+                        const nodeControls = {
+                            left: (threeNodes[0].fatherId ? true : false),
+                            right: (threeNodes[0].fatherId ? true : false),
+                            down: (!nodo.children),
+                            fatherId: val.fatherId,
+                            threeId: val.threeId
+                        };
+                        const newNode = {
+                            innerHTML: UI.toHTMLWithData(Template.stepBox, {
+                                step: nodo,
+                                routeName: 'steps-edit',
+                                nodeControls: nodeControls
+                            })
+                        }
+
+                        if (!nodo.children) return newNode;
+
+                        newNode.children = tranforma(nodo.children).em().diagrama();
+
+                        return newNode;
+                    });
+                    if (!threeNodes[0].fatherId) return mapNode[0];
+
+                    return mapNode;
+                }
+            }
+        }
+    }
+}
+
+function procura(fatherId) {
+
+    return {
+        em(threeNodes) {
+            return {
+                insere(newNode, push) {
+                    const newThree = [];
+                    let stop = false;
+                    threeNodes.forEach((val, ind) => {
+
+                        if (stop) return newThree.push(val);
+
+                        if (val.threeId == fatherId) {
+
+                            if (!val.stepNode.children) {
+                                val.stepNode.children = [];
+                            }
+
+                            if (push) {
+                                val.stepNode.children.push(newNode);
+                            } else {
+                                val.stepNode.children.unshift(newNode);
+                            }
+
+                            newThree.push(val);
+                            stop = true;
+                            return;
+
+                        }
+
+                        if (!val.stepNode.children) {
+                            newThree.push(val);
+                            return;
+                        }
+
+                        val.stepNode.children = procura(fatherId)
+                            .em(val.stepNode.children)
+                            .insere(newNode, push);
+
+                        newThree.push(val);
+
+                    });
+
+                    return newThree;
+                }
+            }
+        }
+    }
+}
