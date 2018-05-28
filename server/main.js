2291cc9e380c7968c416effeeacd949d475d75a2
Gabriel Minatto <gabriel.minatto@deliverit.com.br>
2018-05-28 09:36:34 -0300

    Inserindo funcao no startup que le um settings json para criar contas admin default e arquivo settings com exemplo de estrutura.

--- server/main.js
+++ server/main.js
@@ -2,4 +2,19 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  const defaultAdmins = Meteor.settings.private.defaultAdmins;
+
+  if(!defaultAdmins || !defaultAdmins.length) return;
+
+  defaultAdmins.forEach(({ username, email, password, roles }) => {
+
+    if(Meteor.users.findOne({ "emails.address": email })) return;
+
+    const userId = Accounts.createUser({ email, password, username });
+    if(roles && roles.length){
+
+      Roles.addUsersToRoles(userId, roles);
+    }
+
+  });
 });
