607489388fcc9593a4b0815b506bcf0c3084fd02
Gabriel Minatto <gabriel.minatto@deliverit.com.br>
2018-05-30 16:08:23 -0300

    Adicionando janela com informacoes do projeto ao project see.

--- client/views/public/projects/projectSeeInfo.js
+++ client/views/public/projects/projectSeeInfo.js
@@ -1,6 +1,7 @@
 Template.projectSeeInfo.onCreated(function(){
 
     this.project = new ReactiveVar(false);
+    this.showInfo = new ReactiveVar(false);
 
     subsGlobal.subscribe('allProjectCovers');
     subsGlobal.subscribe('allProjectManuals');
@@ -39,8 +40,21 @@ Template.projectSeeInfo.helpers({
         return ProjectManuals.findOne({ _id: project.manualId });
     },
 
+    showInfo() {
+
+        return Template.instance().showInfo.get();
+    },
+
     formatDescription(desc) {
         return `${desc.slice(0, 100)}${(desc.length > 100 ? '...Mais' : '' )}`
     }
 
+});
+
+Template.projectSeeInfo.events({
+
+    'click .showInfoBtn': function(e, t) {
+
+        t.showInfo.set(!t.showInfo.get());
+    }
 });
