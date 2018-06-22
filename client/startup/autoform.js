a9bb8746dff301df1d5d72973a17b9b049656442
Gabriel Minatto <gabriel.minatto@outlook.com>
2018-06-22 00:56:29 -0300

    Workaround para problema encontrado no autoform onde input files multiple nao aceitavam update.

--- client/startup/autoform.js
+++ client/startup/autoform.js
@@ -6,7 +6,7 @@ const validateOne = (context) => {
         return AutoForm.validateField(doc, key, option);
     }
     return (doc, key, option) => Reflect.apply(func, context, [doc, key, option]);
-}
+};
 
 AutoForm.getValidationContext = function(formId) {
 
@@ -14,4 +14,25 @@ AutoForm.getValidationContext = function(formId) {
     vc.resetValidation = vc.reset;
     vc.validateOne = validateOne(vc);
     return vc;
-}
+};
+
+CfsAutoForm.Util.deepFind = function(obj, path){
+
+    // updates nao recebem createdAt pq esse valor sempre eh setado apenas no insert por motivos obvios
+    const isUpdate = (!obj.createdAt) ? true : false;
+
+    path = path.split('.');
+    let flag = false;
+    for (i = 0; i < path.length - 1; i++) {
+
+        obj = obj[path[i]];
+        flag = true;
+    }
+
+    if (path.length == 1 && !flag && obj[path[0]] && Array.isArray(obj[path[0]]) && isUpdate) {
+
+        obj = obj[path[0]];
+    }
+
+    return obj;
+};
